11063cce3bee9d066438ad8fe59d936e9fea205e
lucasgalleguillo <galleguillolucas2006@gmail.com>
2023-04-20 19:31:25 -0300

    guia3ejercicio4

---
+++ Ejercicios/3_Manejo_de_excepciones_con_python/ejercicio4.py
@@ -0,0 +1,14 @@
+def division():
+        while True:   
+            try:
+                op1 = (int(input("Ingrese un numero: ")))
+                op2 = (int(input("Ingrese otro numero: ")))
+                print(f"El resultado es: {op1 / op2}")
+            except ZeroDivisionError:
+                print("No se puede dividir por cero")
+            except ValueError:
+                print("Ingrese un numero entero")
+
+
+if __name__== "__main__":
+     division()
